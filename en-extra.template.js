bb83c06ae780e8d15643e5e003ddf275dac83221
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2023-04-03 21:02:50 -0400

    Create en-extra.template.js

---
+++ en-extra.template.js
@@ -0,0 +1,18 @@
+module.exports = function (id, label){
+  return {
+    id: id,
+    labels: {
+      "en-us": label,
+      "en-ca": label,
+      "en-au": label
+      "en-gb": label
+      "en-in": label
+    },
+    descriptions: {
+      "en-us": "point in time",
+      "en-ca": "point in time",
+      "en-au": "point in time",
+      "en-gb": "point in time",
+      "en-in": "point in time",
+  }
+}
